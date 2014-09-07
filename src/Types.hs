9d9a21c7f2389cba552c2e334c9d44e3c20e762d
Christopher Biscardi <biscarch@sketcht.com>
2014-09-07 15:10:29 -0700

    Clean up

--- src/Types.hs
+++ src/Types.hs
@@ -1,15 +1,12 @@
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE OverloadedStrings #-}
 
-module Types 
+module Types
   ( Text32(unText32)
-  , text32
   , Text64(unText64)
-  , text64
-  , AppValidation
   , ListValidation
   , VError(..)
-  , withArraySequenceA
+--  , withArraySequenceA
   , withArraySeqList
   ) where
 
@@ -17,14 +14,11 @@ import           Control.Applicative
 import           Control.Lens
 import           Data.Aeson
 import           Data.Aeson.Types
-import           Data.Validation
-import qualified Data.Text as T
-import qualified Data.Traversable as TR
-import qualified Data.Vector as V
 import           Data.Semigroup
-
--- Alias to make types shorter
-type AppValidation a = AccValidation [VError] a
+import qualified Data.Text           as T
+import qualified Data.Traversable    as TR
+import           Data.Validation
+import qualified Data.Vector         as V
 
 data VError = MustNotBeEmpty T.Text
             | MustBeLessThanLength32 T.Text
@@ -33,37 +27,13 @@ data VError = MustNotBeEmpty T.Text
 
 -- Basic validated types with smart constructors
 newtype Text32 = Text32 { unText32 :: T.Text } deriving (Eq, Show)
- 
-text32 :: T.Text -> AppValidation Text32
-text32 t
-  | T.length t == 0   = _Failure # [MustNotBeEmpty t]
-  | T.length t <= 32  = _Success # Text32 t
-  | otherwise         = _Failure # [MustBeLessThanLength32 t]
 
 -- The following can be cleaned by extracting common code
 newtype Text64 = Text64 { unText64 :: T.Text } deriving (Eq, Show)
- 
-text64 :: T.Text -> AppValidation Text64
-text64 t
-  | T.length t == 0   = _Failure # [MustNotBeEmpty t]
-  | T.length t <= 64  = _Success # Text64 t
-  | otherwise         = _Failure # [MustBeLessThanLength64 t]
-
--- aeson instances
-instance FromJSON (AppValidation Bool) where
-  parseJSON = withBool "Bool" $ pure . pure
-
-instance FromJSON (AppValidation Text32) where
-  parseJSON = withText "Text32" $ pure . text32
-
-instance FromJSON (AppValidation Text64) where
-  parseJSON = withText "Text64" $ pure . text64
-
-
 
 -- aeson helper for [f a] -> f [a]
-withArraySequenceA :: (FromJSON (f a), Applicative f) => String -> Value -> Parser (f [a])
-withArraySequenceA s = withArray s $ fmap TR.sequenceA . mapM parseJSON . V.toList
+-- withArraySequenceA :: (FromJSON (f a), Applicative f) => String -> Value -> Parser (f [a])
+-- withArraySequenceA s = withArray s $ fmap TR.sequenceA . mapM parseJSON . V.toList
 
 withArraySeqList s = withArray s $ fmap TR.sequenceA . mapM parseJSON . V.toList
 
@@ -77,7 +47,7 @@ data ListError = ListError
   } deriving (Show)
 
 instance Semigroup (ListError) where
-  (<>) a b = ListError (me_title a <> e_title b)
+  (<>) a b = ListError (e_title a <> e_title b)
                        (e_owner a <> e_owner b)
                        (e_items a <> e_items b)
 
